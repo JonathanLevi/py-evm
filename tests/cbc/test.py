5c39fc70fa340abef646acf8018d83aa1e400f82
Shreyas Bhat <shreyasnbhat@gmail.com>
2018-12-09 04:53:55 +0800

    Updated the test.py simulator

--- tests/cbc/test.py
+++ tests/cbc/test.py
@@ -12,6 +12,7 @@ from config import (
     SHARDS_CONFIG,
     MAGIC_PRI_KEY,
     MAGIC_ADDRESS,
+    SHARD_IDS
 )
 
 klass = MiningChain.configure(
@@ -36,36 +37,39 @@ tx = vm.create_unsigned_transaction(
 
 sender_pri_key = keys.PrivateKey(decode_hex(MAGIC_PRI_KEY))
 
-signed_tx = tx.as_signed_transaction(sender_pri_key)
-chain.apply_transaction(signed_tx)
 
-# --------------
+chains = {}
+vms = {}
+for i in SHARD_IDS:
+    chains[i] = klass.from_genesis(AtomicDB(), SHARDS_CONFIG[i]['GENESIS_PARAMS'])
+    vms[i] = chains[i].get_vm()
 
-vm = chain.get_vm()
-nonce = vm.state.account_db.get_nonce(MAGIC_ADDRESS)
-tx = vm.create_unsigned_transaction(
+
+for i in SHARD_IDS:
+    print("============")
+    print("Shard ", i)
+    print("=============")
+    for j in range(1,len(SHARDS_CONFIG[i]['ADDRESSES'])):
+        nonce = vms[i].state.account_db.get_nonce(MAGIC_ADDRESS)
+        tx = vms[i].create_unsigned_transaction(
             nonce=nonce,
             gas_price=0,
             gas=100000,
-            to=SHARDS_CONFIG[0]['ADDRESSES'][0],
+            to=SHARDS_CONFIG[0]['ADDRESSES'][j],
             value=0,
             data="Test TX data".encode('utf-8'),
         )
 
-signed_tx = tx.as_signed_transaction(sender_pri_key)
-chain.apply_transaction(signed_tx)
+        sender_address = SHARDS_CONFIG[i]['ADDRESSES'][0]
+        block = vms[i].finalize_block(chains[i].get_block())
+        nonce, mix_hash = mine_pow_nonce(
+            block.number,
+            block.header.mining_hash,
+            block.header.difficulty
+        )
+        print(chains[i].mine_block(mix_hash=mix_hash, nonce=nonce))
+        print("Sender:",sender_address)
+        print("Receiver:",SHARDS_CONFIG[0]['ADDRESSES'][j])
+        current_block = chains[i].get_block_by_header(chains[i].get_canonical_head())
+        print("Current Block:", current_block)
 
-# --------------
-
-block = chain.get_vm().finalize_block(chain.get_block())
-nonce, mix_hash = mine_pow_nonce(
-                        block.number,
-                        block.header.mining_hash,
-                        block.header.difficulty
-                    )
-print(chain.mine_block(mix_hash=mix_hash, nonce=nonce))
-
-current_block = chain.get_block_by_header(chain.get_canonical_head())
-print("Current Block:", current_block)
-
-print(block.header.shard_id)
